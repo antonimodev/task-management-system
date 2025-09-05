9424be0ba348aece00450e136cf8e565c3ef3fb9
antonimo <antonio98park@hotmail.com>
2025-09-05 09:43:17 +0200

    Create DJANGO_SECRET_KEY as env variable, DRF added and update dependencies
    - Move SECRET_KEY to environment variable in settings.py for security.
    - Add Django REST Framework (djangorestframework) to requirements.txt.
    - Add 'rest_framework' to INSTALLED_APPS in settings.py.
    - Adjust Django version in requirements.txt from 4.2+ to 4.2 for consistency.

--- django_backend/config/settings.py
+++ django_backend/config/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)qd(e_8iyk*$rakl&=s6t%btn!g88gvv30p44bnn_hy*b)d(8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+	'rest_framework', # DRF (Django REST Framework)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
